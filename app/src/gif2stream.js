851551e6f1abe476f6784ee5783b47d1a588b1a8
Jonathan Leek <mrjonleek@gmail.com>
2021-09-15 11:51:26 -0700

    Mass commit.

--- app/src/gif2stream.js
+++ app/src/gif2stream.js
@@ -17,6 +17,7 @@ const gif2array = async (src) => extractFrames({ input: src })
 const array2stream = async (frames) => {
   return new Promise( resolve => {
     const { byteOffset, byteLength } = frames.data;
+    // TODO: Iterate over frames and return to be passed to Kafka
     const buffer = frames.data.buffer.slice(byteOffset, byteLength + byteOffset)
     const stream = streamifier.createReadStream(buffer);
     resolve(stream);
